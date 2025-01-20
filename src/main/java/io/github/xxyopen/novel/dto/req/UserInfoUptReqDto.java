83607824f86c843ef9c7db68d7ab1c187395cc67
wovodedrama <1622813370@qq.com>
2025-01-20 19:25:22 +0800

    perf: change the scope of nickname
    - 将用户昵称上限改为11，因为默认昵称为用户手机号，手机号长度为11，统一长度范围

--- src/main/java/io/github/xxyopen/novel/dto/req/UserInfoUptReqDto.java
+++ src/main/java/io/github/xxyopen/novel/dto/req/UserInfoUptReqDto.java
@@ -18,11 +18,11 @@ public class UserInfoUptReqDto {
     private Long userId;
 
     @Schema(description = "昵称")
-    @Length(min = 2,max = 10)
+    @Length(min = 2,max = 11)
     private String nickName;
 
     @Schema(description = "头像地址")
-    @Pattern(regexp="^/[^\s]{10,}\\.(png|PNG|jpg|JPG|jpeg|JPEG|gif|GIF|bpm|BPM)$")
+    @Pattern(regexp= "^/[^ ]{10,}\\.(png|PNG|jpg|JPG|jpeg|JPEG|gif|GIF|bpm|BPM)$")
     private String userPhoto;
 
     @Schema(description = "性别")
